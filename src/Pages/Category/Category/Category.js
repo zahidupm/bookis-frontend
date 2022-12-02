43094b85826ddc3313a0a6fc71a97cb7d39f95f8
zahidupm <zahidhasanlimon111@gmail.com>
2022-12-02 12:05:22 +0600

    categoryId modal not open fixed

--- src/Pages/Category/Category/Category.js
+++ src/Pages/Category/Category/Category.js
@@ -3,6 +3,7 @@ import { useLoaderData } from 'react-router-dom';
 import '../../../index.css';
 import Loading from '../../Shared/Loading/Loading';
 import CategoriesItem from '../Categories/CategoriesItem';
+import CategoryModal from '../Categories/CategoryModal';
 import RightSideNav from '../RightSideNav/RightSideNav';
 
 const Category = () => {
@@ -22,7 +23,15 @@ const Category = () => {
     }
     
     return (
-        <div className='flex'>
+        <>
+        {
+                treatment && 
+                <CategoryModal 
+                treatment={treatment}
+                setTreatment={setTreatment}
+                ></CategoryModal>
+            }
+            <div className='flex'>
             <div className='w-3/4 bg-gray-100'>
                 <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
                 <div className="grid gap-5 lg:grid-cols-2 sm:max-w-sm sm:mx-auto lg:max-w-full">
@@ -40,6 +49,7 @@ const Category = () => {
                 <RightSideNav></RightSideNav>
             </div>
         </div>
+        </>
     );
 };
 
